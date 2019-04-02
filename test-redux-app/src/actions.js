a19220743b8db16924193dd1fdb90df5a5a1f5b2
Philippe Recto <precto1285@gmal.com>
2019-04-02 13:30:34 -0700

    added action.js

---
+++ test-redux-app/src/actions.js
@@ -0,0 +1,15 @@
+export const ADD_TODO = 'ADD_TODO'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+
+export const VisibilityFilters = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+
+export function addTodo(text) { return }
+
+export function toggleTodo(index) { return }
+
+export function setVisibilityFilter(filter) { return }
