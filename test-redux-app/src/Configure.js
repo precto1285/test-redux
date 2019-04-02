f94cb9b4657976a3d1363af30e475f74c4b2f024
Philippe Recto <precto1285@gmal.com>
2019-04-02 10:48:43 -0700

    added redux dev tools ext and hot reloading

--- test-redux-app/src/Configure.js
+++ test-redux-app/src/Configure.js
@@ -1,5 +1,6 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import monitorReducersEnhancer from './enhancers/monitorReducers';
 import loggerMiddleware from './middleware/logger';
 import rootReducer from './reducers'
@@ -9,9 +10,13 @@ const configureStore = preloadedState => {
   const middlewareEnhancer = applyMiddleware(...middlewares)
 
   const enhancers = [middlewareEnhancer, monitorReducersEnhancer]
-  const composedEnhancers = compose(...enhancers)
+  const composedEnhancers = composeWithDevTools(...enhancers)
 
   const store = createStore(rootReducer, preloadedState, composedEnhancers)
+
+  if (process.env.NODE_ENV !== 'production' && module.hot) {
+    module.hot.accept('./reducers', () => store.replaceReducer(rootReducer))
+  }
 
   return store
 }
